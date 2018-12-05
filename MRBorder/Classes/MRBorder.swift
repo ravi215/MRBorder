2049b8c0a7dd22ee221b0863d9e5f6fcd0fa6203
Ravi Bhooshan <ravibhooshan@Ravis-MacBook-Air.local>
2018-12-05 17:37:53 +0530

    seconds commit

--- MRBorder/Classes/MRBorder.swift
+++ MRBorder/Classes/MRBorder.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension UIImageView {
+    public func borderMe(borderColor: UIColor, borderWidth: CGFloat) {
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.cornerRadius = 5.0
+        self.layer.borderWidth = borderWidth
+        self.clipsToBounds = true
+        
+    }
+}
